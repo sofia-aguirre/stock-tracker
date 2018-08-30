1e64132912ce0f57666757cd64c10f73691b8bde
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-08-30 11:16:29 -0700

    added Yi's tracker button functions to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,7 +18,29 @@ $('#login-button').on('click', function(event) {
 ///////END OF LANDING BUTTONS//////////////////////
 
 
-
+/////////TRADE FORM BUTTONS//////////
+// listen to 'tradeForm' 'addToLog' button that submit form data,
+// (as placeholder) select 'divLog' and append form data to array
+var logArr = [];
+var newLog;
+$('#tradeForm').on('click', '#addToLog', function(e){
+    e.preventDefault();
+    // serializeArray return array of object instead of string
+    var formDataArr = $(this).parent().serializeArray();
+    // console.log('clicked add to log button, form data array: ', formDataArr);
+    var symbolData = formDataArr.filter(formData => formData.name == 'symbol')[0].value;
+    var shareData = formDataArr.filter(formData => formData.name == 'share')[0].value;
+    var dateData = formDataArr.filter(formData => formData.name == 'date')[0].value;
+    var boughtOrSoldData = formDataArr.filter(formData => formData.name == 'boughtOrSold')[0].value;
+    newLog = `${symbolData}&nbsp;&nbsp;&nbsp;&nbsp;${boughtOrSoldData}&nbsp;&nbsp;&nbsp;&nbsp;${shareData}&nbsp;&nbsp;&nbsp;&nbsp;${dateData}&nbsp;&nbsp;&nbsp;&nbsp;(# of share * price at that date)`;
+    // console.log(newLog);
+    logArr.push(newLog);
+    console.log(logArr);
+    $('#divLog').append(`
+        <p>${newLog}</p>
+    `);
+});
+/////////END OF TRADE FORM BUTTONS//////////
 
 
 
