2018c25e4d8be27103c93d7930a45792a46875c1
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-08-30 11:11:48 -0700

    landing page buttons JS show signup/login form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,4 +2,25 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+
+///////LANDING BUTTONS////////
+// shows the signup form when either buttons are clicked
+$('#signup-button').on('click', function(event) {
+    event.preventDefault();
+    $('#form-wrapper').removeClass('hidden');
+});
+
+// shows the login form when either buttons are clicked
+$('#login-button').on('click', function(event) {
+    event.preventDefault();
+    $('#form-wrapper').removeClass('hidden');
+});
+///////END OF LANDING BUTTONS//////////////////////
+
+
+
+
+
+
+// end of document.ready
 });
