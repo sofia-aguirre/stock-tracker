fb86ead0c7bda4d2ac1de47688a0a973db34da03
fruit13ok <fruit13ok@gmail.com>
2018-08-31 14:22:04 -0700

    created user signup post to database that check duplicate user

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3000');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3000');
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/stock-tracker", {useMongoClient: true});
 
 module.exports.User = require('./user.js');
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // allow cross origin requests (optional)
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -35,9 +35,9 @@ app.use(express.static('public'));
  * HTML Endpoints
  */
 app.get('/', function homepage(req, res) {
-    res.sendFile(__dirname + '/views/landing.html');
-    // res.sendFile(__dirname + '/views/stockTracker.html');
-  });
+  res.sendFile(__dirname + '/views/landing.html');
+  // res.sendFile(__dirname + '/views/stockTracker.html');
+});
 
 /*
  * JSON API Endpoints
@@ -46,7 +46,26 @@ app.post('/signup', function signup(req, res) {
   var signupForm = req.body;
   var formEmail = signupForm.email;
   var formPassword = signupForm.password;
-  
+
+
+  db.User.find({ email: formEmail }, function (err, foundUser) {
+    if (err) { res.json({ err }) }
+    // if foundUser's length >= 1
+    // send a response back to user that email already exists
+    // if foundUser's length === 0
+    // we need to create a new user
+    if (foundUser.length >= 1) {
+      res.json({ message: 'email already exists' });
+    } else {
+      db.User.create({ email: formEmail, password: formPassword },
+        function (err, createdUser) {
+          if (err) {
+            res.json({ err });
+          }
+          res.json(createdUser);
+        });
+    }
+  });
 })
 
 
@@ -56,5 +75,5 @@ app.post('/signup', function signup(req, res) {
 
 // listen on the port that Heroku prescribes (process.env.PORT) OR port 3000
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Express server is up and running on http://localhost:3000/');
+  console.log('Express server is up and running on http://localhost:3000/');
 });
