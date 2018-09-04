f609522a15019f35da55cbeab886bbb3f29d1db0
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-09-04 10:10:45 -0700

    fixed useMongoClient index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/stock-tracker10",  { useNewUrlParser: true });
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/stock-tracker10",  { useMongoClient: true });
 
 module.exports.User = require('./user.js');
 module.exports.Trade = require('./trade.js');
